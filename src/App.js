872be98dc456ba0f3f67eb2295e82b80a84d9cc3
Andrea Astorga <andrea.astorga95@gmail.com>
2021-03-10 19:15:48 +0100

    Leap years Kata first 12 tests, only .toBe()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// All years divisible by 400 ARE leap years 
+// (so, for example, 2000 was indeed a leap year),
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// All years divisible by 100 but not by 400 are NOT leap years 
+// (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+
+// All years divisible by 4 but not by 100 ARE leap years 
+// (e.g., 2008, 2012, 2016),
+
+// All years not divisible by 4 are NOT leap years 
+// (e.g. 2017, 2018, 2019).
+
+export class App {
+
+    calendarLeapYears (year) {
+
+      if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        return 'Leap year' 
+      } else if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) {
+        return 'Not leap year'
+      } 
+
+  }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App tests', () => {
+  const app = new App();
+
+  it ('Test 1 YES: Should print => Leap year', () => {
+    const response = app.calendarLeapYears(2000);
+    expect(response).toBe('Leap year')
+  })
+
+  it ('Test 2 YES: Should print => Leap year', () => {
+    const response = app.calendarLeapYears(2008);
+    expect(response).toBe('Leap year')
+  })
+
+  it ('Test 4 YES: Should print => Leap year', () => {
+    const response = app.calendarLeapYears(2012);
+    expect(response).toBe('Leap year')
+  })
+
+  it ('Test 5 YES: Should print => Leap year', () => {
+    const response = app.calendarLeapYears(2016);
+    expect(response).toBe('Leap year')
+  })
+
+  it ('Test 1 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(2);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 2 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(1700);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 3 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(1800);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 4 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(1900);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 5 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(2100);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 6 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(2017);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 7 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(2018);
+    expect(response).toBe('Not leap year')
+  })
+  it ('Test 8 NO: Should print => Not leap year', () => {
+    const response = app.calendarLeapYears(2019);
+    expect(response).toBe('Not leap year')
+  })
+
+})
